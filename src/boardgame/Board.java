7080bc6b780290fe49ea3448862b778d4a747195
jvtuta <joaovoliveira2399@gmail.com>
2023-01-24 20:03:39 -0300

    add board and board piece

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -1,5 +1,24 @@
 package boardgame;
 
 public class Board {
-  
+  private Piece[][] pieces;
+  private int rows, columns;
+
+  public Board(int rows, int columns) {
+    this.rows = rows;
+    this.columns = columns;
+    pieces = new Piece[rows][columns];
+  }
+
+  public int getRows()
+  {
+    return rows;
+  }
+
+  public int getColumns()
+  {
+    return columns;
+  }
+
+
 }
--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -1,5 +1,29 @@
 package boardgame;
 
 public class Piece {
+  Position position;
+  Board board;
   
+  public Piece(Board board)
+  {
+    // initialy position will be null because piece will be not in the board
+    this.position = null;
+    this.board = board;
+  }
+
+  protected Board getbBoard()
+  {
+    return board;
+  }
+
+  protected Position getPosition()
+  {
+    return position;
+  }
+
+  protected void setPosition(Position position)
+  {
+    this.position = position;
+  }
+
 }
